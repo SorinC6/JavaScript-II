0a284e6d0805fe56ed1b47f6940500b1b3012abf
Sorin Chis <kish.sorin@yahoo.com>
2019-01-22 16:06:50 +0200

    task 4 completed plus strech challange

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,21 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
-const greet= "Hey I'm Sorin";
+const greet = "Hey I'm Sorin";
 console.log(greet);
 
-function parent(){
+function parent() {
     //debugger;
-    const parentHello="Hey I'm a parent";
+    const parentHello = "Hey I'm a parent";
     console.log(parentHello);
-    function grandParent(parentHello){
+    function grandParent(parentHello) {
         //debugger;
-        const grandParentHello="Hey I'm a grandParent";
+        const grandParentHello = "Hey I'm a grandParent";
         console.log(parentHello);
         console.log(grandParentHello);
-        function grandgrandParent(){
+        function grandgrandParent() {
             //debugger;
-            const grandgrandParentHello="Hey I'm a grandgrandParent";
+            const grandgrandParentHello = "Hey I'm a grandgrandParent";
             console.log(grandgrandParentHello);
             console.log(grandParentHello);
             console.log(parentHello)
@@ -29,23 +29,53 @@ parent();
 console.log('==========================================')
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-    let x=0;
-  // Return a function that when invoked increments and returns a counter variable.
-  return function newCounter(){
-      return x++;
-  }
+    let x = 0;
+    // Return a function that when invoked increments and returns a counter variable.
+    return function newCounter() {
+        return ++x;
+    }
 };
 const newCounter = counter();
- newCounter(); // 1
-// newCounter(); // 2
+console.log(newCounter());
+console.log(newCounter());
 
 
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
-};
+// const counterFactory = () => {
+//     let cont=0;
+//     return {
+//         increment: function(){
+//             ++cont;
+//         },
+//         decrement: function(){
+//             --cont;
+//         }
+//     }
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
+//};
+
+const CounterFactory = () => objectFunction = {
+    counter : 0,
+    increment: function(){
+        ++this.counter;
+        return this.counter;
+    },
+    decrement: function(){
+        --this.counter;
+        return this.counter;
+    },
+}
+
+
+console.log('=======================')
+
+const nCounter = CounterFactory();
+
+console.log(nCounter.increment());
+console.log(nCounter.increment());
+console.log(nCounter.decrement());
